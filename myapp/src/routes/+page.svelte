d91f0e212b00757d20e1f99684ecaf9009f1b741
Harry Shoemaker <shoemaker323@gmail.com>
2023-01-14 10:05:46 -0600

    initial commit

---
+++ myapp/src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+    import 'bulma/css/bulma.css';
+</script>
+
+<section class="section">
+    <div class="container">
+        <h1 class="title">Welcome to SvelteKit</h1>
+        <p class="subtitle">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+        <button class="button is-large is-primary">Button</button>
+        <h2 class="title is-2">Learn to customize Bulma with either Sass or WebPack</h2>
+    </div>
+</section>
+
+<style>
+    :global(html) {
+        background-color: darkgrey;
+    }
+</style>
