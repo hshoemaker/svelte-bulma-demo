3741a004ae6dded3d828780d4ee3a163db6cfbc9
Harry Shoemaker <shoemaker323@gmail.com>
2023-01-17 09:58:38 -0600

    trying layout to globally add styles

---
+++ myapp/src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+    // import '../styles/main.css';
+    import 'bulma/css/bulma.css';
+</script>
+
+<style global>
+    html {
+        background-color: darkgrey;
+    }
+</style>
+
+<slot />
--- myapp/src/routes/+page.svelte
+++ myapp/src/routes/+page.svelte
@@ -1,18 +1,24 @@
 <script>
-    import 'bulma/css/bulma.css';
+    // import 'bulma/css/bulma.css';
+
+	import Layout from "./+layout.svelte";
+
 </script>
 
-<section class="section">
-    <div class="container">
-        <h1 class="title">Welcome to SvelteKit</h1>
-        <p class="subtitle">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-        <button class="button is-large is-primary">Button</button>
-        <h2 class="title is-2">Learn to customize Bulma with either Sass or WebPack</h2>
-    </div>
-</section>
+<Layout>
+    <section class="section">
+        <div class="container">
+            <h1 class="title">Welcome to SvelteKit</h1>
+            <p class="subtitle">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+            <button class="button is-large is-primary">Button</button>
+            <h2 class="title is-2">Learn to customize Bulma with either Sass or WebPack</h2>
+        </div>
+    </section>
 
-<style>
+
+<!-- <style>
     :global(html) {
         background-color: darkgrey;
     }
-</style>
+</style> -->
+</Layout>
